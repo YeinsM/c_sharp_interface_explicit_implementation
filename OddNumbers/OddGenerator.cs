cb08e223ee0353fc29a1c9883a68648ffacbf3af
YeinsM <wildermancera1@gmail.com>
2022-04-18 12:28:02 -0400

    Sample for conflict member with IEnumerable, IEnumerator meethods

--- OddNumbers/OddGenerator.cs
+++ OddNumbers/OddGenerator.cs
@@ -3,7 +3,22 @@ using System.Collections.Generic;
 
 namespace OddNumbers
 {
-    public class OddGenerator
+    public class OddGenerator : IEnumerable<int>
     {
+        public IEnumerator<int> GetEnumerator()
+        {
+            int i = 1;
+            yield return i;
+            while (true)
+            {
+                i += 2;
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
