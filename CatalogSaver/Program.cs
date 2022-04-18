e06bdc2b7de5cfd0e7d0159e66ef571248b86e7a
YeinsM <wildermancera1@gmail.com>
2022-04-18 11:54:12 -0400

    Add doc comments

--- CatalogSaver/Program.cs
+++ CatalogSaver/Program.cs
@@ -12,10 +12,12 @@
             //var varCatalog = new Catalog();
             //varCatalog.Save();
 
+            //create variable with interface type
             ISaveable saveable = new Catalog();
             saveable.Save();
             //"Saved"
 
+            //convert Catalog to ISaveable
             ((ISaveable)catalog).Save();
             //"Saved"
         }
